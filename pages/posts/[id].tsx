e0ebb951e5990ed1b1671aecaa272c616c90aed7
HasanTusher <hasan.tusher787@gmail.com>
2021-12-20 12:14:44 +0600

    static code paths generation done

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,13 +1,43 @@
 import {GetStaticPaths, GetServerSideProps, GetStaticProps} from "next";
 
-// export const getStaticProps: GetStaticProps = async (context) => {
-//
-// }
-//
-// export const getStaticPaths: GetStaticPaths = async () => {
-//     // ...
-// }
-//
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//     // ...
-// }
+import { ParsedUrlQuery } from 'querystring'
+
+interface IParams extends ParsedUrlQuery {
+    id: string
+}
+
+/**
+ * return the component
+ * @param post
+ * @constructor
+ */
+function Post({ post}) {
+    return <div>{post.data}</div>
+}
+
+/**
+ * generates the static props
+ * @param context
+ */
+export const getStaticProps: GetStaticProps = async (context) => {
+    const  slug  = context.params as IParams;
+    const post = {
+     'data':  `/api/${slug.id}`
+    };
+    return {props: {post}}
+}
+
+/**
+ * mock dataset
+ */
+export const getStaticPaths: GetStaticPaths = async () => {
+    const arr: string[] = ['slug1', 'slug2']
+    const paths = arr.map((slug) => {
+        return {
+            params: { id: slug },
+        }
+    })
+    return { paths, fallback: false }
+}
+
+export default Post
